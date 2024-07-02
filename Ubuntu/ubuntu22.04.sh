cd48798ef46fd5542eb5d87259983c22bb9bc03d
Beta Lin <sb2828sb@gmail.com>
2024-07-03 00:05:52 +0800

    start over from here

--- Ubuntu/ubuntu22.04.sh
+++ Ubuntu/ubuntu22.04.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-apt upgrade -y
-apt update -y
+sudo apt upgrade && sudo apt update -y
+
+# Install some useful program
+sudo apt install build-essential file -y
+sudo apt install docker.io nginx zsh -y
 
 
